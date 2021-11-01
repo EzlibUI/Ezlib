1b2759ed7c24835c15a4e3bd1a18630e32593f20
Daniel <dj.tarsalainen@hotmail.com>
2021-11-01 12:15:59 +0200

    new updates

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -7,15 +7,7 @@ export const Modal = ({ show, toggle, content }) => {
       return (
           <div className="modal">
               <button onClick={() => toggle()} id="exitModal">X</button>
-              {/* <button onClick={() => toggle()} id="exitModal">X</button>
-              <div className="modal-container">
-                  <input type="text" placeholder="Email" />
-                  <br />
-                  <input type="password" placeholder="Password" />
-                  <br />
-                  <a href="url">Forgot password?</a>
-                  <button variant="primary">Sign up</button>
-              </div> */}
+
               <div className="modal-container">{content}</div>
           </div>
       )
