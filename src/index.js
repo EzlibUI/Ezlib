a1f96de5b5dd9b2c5275618779159212970e06e6
Oliver Hyryläinen <itzkinkkuu@gmail.com>
2021-11-21 15:30:48 +0200

    added themeswitch to exports

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 export * from './components/Table/'
-<<<<<<< HEAD
-=======
 export * from './components/Button/'
 export * from './components/Navbar/'
->>>>>>> aed24c22e5c1d225565fc111bc92755b23582294
+export * from './components/ThemeSwitch/'
