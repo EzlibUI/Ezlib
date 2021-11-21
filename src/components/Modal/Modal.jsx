39592dc4161ac39ece9e286b42801fa759062984
Oliver Hyryläinen <itzkinkkuu@gmail.com>
2021-11-21 14:45:58 +0200

    fixed modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,23 +1,27 @@
-import React from 'react';
+import React from "react";
 
 export const Modal = ({ show, toggle, content }) => {
-    //show-tila määrittää, onko modaali näkyvissä vai ei. Sen tila sijaitsee ylemmässä komponentissa
-    if (!show) {
-        return null;
-    } else {
-        return (
-            <div className="background-blur">
-                <div className="modal">
-                    <button onClick={() => toggle()} id="exitModal">X</button>
-                    <div className="modal-container">{content}</div>
-            <div className="modal">
-                {/* toggle on funktio, joka vaihtaa modaalin näkyvyyden tilaa. funktion määrittely tällä hetkellä sijaitsee ylemmässä */}
-                <button onClick={() => toggle()} id="exitModal">X</button>
-                {/* modal-container ottaa vastaan jsx-koodia content-propsin kautta. Content propsin koodi tulee esille modaali-ikkunassa*/}
-                <div className="modal-container">
-                    {content}
-                </div>
-            </div>
-        )
-    }
-}
+  //show-tila määrittää, onko modaali näkyvissä vai ei. Sen tila sijaitsee ylemmässä komponentissa
+  if (!show) {
+    return null;
+  } else {
+    return (
+      <div className="background-blur">
+        <div className="modal">
+          <button onClick={() => toggle()} id="exitModal">
+            X
+          </button>
+          <div className="modal-container">{content}</div>
+          <div className="modal">
+            {/* toggle on funktio, joka vaihtaa modaalin näkyvyyden tilaa. funktion määrittely tällä hetkellä sijaitsee ylemmässä */}
+            <button onClick={() => toggle()} id="exitModal">
+              X
+            </button>
+            {/* modal-container ottaa vastaan jsx-koodia content-propsin kautta. Content propsin koodi tulee esille modaali-ikkunassa*/}
+            <div className="modal-container">{content}</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+};
--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 export * from './Table'
-=======
-export * from './Table'
->>>>>>> aed24c22e5c1d225565fc111bc92755b23582294
